89c1406413179a8d9aea58a277f156bee500d48f
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2026-09-11 17:25:56 -0500

    missed extra bracket

--- tests/test_mod01_04.py
+++ tests/test_mod01_04.py
@@ -6,7 +6,7 @@ def test_matAdd():
     
 def test_scalarMatMult():
     assert numpy.allclose(scalarMatMult(3j,[[1,3,4],[4,5,6j],[7,8,9]]), [[(0+3j), (0+9j), (0+12j)], [(0+12j), (0+15j), (-18+0j)], [(0+21j), (0+24j), (0+27j)]])
-    assert numpy.allclose(scalarMatMult([4,[[9,8,7j],[6,5,4],[3,2j,1]]), [[(36+0j), (32+0j), (0+28j)], [(24+0j), (20+0j), (16+0j)], [(12+0j), (0+8j), (4+0j)]])
+    assert numpy.allclose(scalarMatMult(4,[[9,8,7j],[6,5,4],[3,2j,1]]), [[(36+0j), (32+0j), (0+28j)], [(24+0j), (20+0j), (16+0j)], [(12+0j), (0+8j), (4+0j)]])
 
 def test_transpose():
     assert numpy.allclose(transpose([[1,3,4],[4,5,6j],[7,8,9]]), [[1, 4, 7], [3, 5, 8], [4, 6j, 9]])
